f65a324d3b6c51703078f74c3940dd58b7f1b4aa
Renish-patel <renishpatel2482001@gmail.com>
2023-09-26 17:45:01 +0530

    global 404 page , you can also create the specific 404 page using [...name-of-page] inside it create a folder page.js and create the specific 404 page using route

---
+++ src/app/not-found.js
@@ -0,0 +1,8 @@
+export default function PageNotFoundError(){
+    return(
+        <div class="error">404
+<span class="info">File not found</span>
+<img src="http://images2.layoutsparks.com/1/160030/too-much-tv-static.gif" class="static" />
+</div>
+        )
+}
